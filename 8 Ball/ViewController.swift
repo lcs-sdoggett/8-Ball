f0a7816bd097a5f78a6d4da84e1de5d4638bfc41
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-24 21:29:13 -0400

    logic finished

--- 8 Ball/ViewController.swift
+++ 8 Ball/ViewController.swift
@@ -23,14 +23,23 @@ class ViewController: UIViewController {
         let randomNumber = Int.random(in: 1...8)
         
         switch randomNumber {
-        case 1: mainLabel.text = "Its wings are too small to get its fat little body off the ground."
+        case 1: mainLabel.text = "A show’s first episode is called a pilot because it’s what puts the show on the air"
             
-        case 2: mainLabel.text = "This app is pretty cool"
+        case 2: mainLabel.text = "A lethal dose is also a lifetime supply"
             
-        case 3: mainLabel.text = "I have no "
-
-        default:
-            mainLabel.text = "1"
+        case 3: mainLabel.text = "We're all half sentars"
+            
+        case 4: mainLabel.text = "When a pregnent women swims, she's like a human submarine"
+            
+        case 5: mainLabel.text = "In a room filled with only you and a bunch of mirrors, what would you see?"
+            
+        case 6: mainLabel.text = "Blicking is like clapping for your eyes"
+            
+        case 7: mainLabel.text = "People that study atoms are just a bunch of atoms trying to understand themselves"
+            
+        case 8: mainLabel.text = "Patrick is clueless because he lives under a rock"
+            
+        default: mainLabel.text = ""
         }
         
     }
