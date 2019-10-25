bf8514223e4a9ca63290af27050db0de5ed898b9
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-24 21:12:38 -0400

    finished the view and some cases

--- 8 Ball/ViewController.swift
+++ 8 Ball/ViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//MARK: Properties
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+
+//MARK: Methods
+
+    // Show message to user
+    @IBAction func Shake(_ sender: Any) {
+        
+        let randomNumber = Int.random(in: 1...8)
+        
+        switch randomNumber {
+        case 1: mainLabel.text = "Its wings are too small to get its fat little body off the ground."
+            
+        case 2: mainLabel.text = "This app is pretty cool"
+            
+        case 3: mainLabel.text = "I have no "
+
+        default:
+            mainLabel.text = "1"
+        }
+        
+    }
+    
+
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +46,4 @@ class ViewController: UIViewController {
 
 }
 
+}
