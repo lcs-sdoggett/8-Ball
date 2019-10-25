401bf324a7f2754d30b1cea94b07903804c418f4
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-24 20:47:00 -0400

    Initial Commit

---
+++ 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8 Ball
+//
+//  Created by Doggett, Scott on 2019-10-24.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
